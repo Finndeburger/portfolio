395ec39e029a3ae86c3ad89eb8a9497f3af5c351
Finndeburger <106623336+Finndeburger@users.noreply.github.com>
2026-03-21 15:42:37 +0000

    Laravel Setup

--- app/layouts/default.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="min-h-screen bg-[#e4e4e4]">
-    <slot />
-  </div>
-</template>
--- app/pages/sites/test.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-definePageMeta({ layout: 'browser' })
-</script>
-
-<template>
-    <div class="p-10">
-        <h1 class="text-3xl font-bold mb-4">Hallo, test test.</h1>
-    </div>
-</template>
