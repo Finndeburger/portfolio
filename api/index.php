af59d44378bd2321d2ecf1234da34b04988b5916
Finndeburger <106623336+Finndeburger@users.noreply.github.com>
2026-05-15 22:50:00 +0000

    Add PHP error display to diagnose 500

--- api/index.php
+++ api/index.php
@@ -5,6 +5,11 @@
 $_SERVER['HTTPS'] = 'on';
 $_SERVER['SERVER_PORT'] = '443';
 
+// Capture PHP errors to output in debug mode
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 // Create required temp directories for serverless environment
 $dirs = [
     '/tmp/storage/framework/views',
